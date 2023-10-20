ac0e705ba5a8d5dbb41f60987b00b110890d0ae2
matheanna <113627409+matheanna@users.noreply.github.com>
2023-10-20 17:57:56 +0200

    build: create react app

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        t5drgzuihjok
+      </header>
+    </div>
+  );
+}
+
+export default App;
