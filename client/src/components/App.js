c4334e7b00806f136222874271cb1007e4ea4005
matheanna <113627409+matheanna@users.noreply.github.com>
2023-10-30 10:03:58 +0100

    feat: go to register/home, searchbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router-dom";
-import '../style/App.css';
+import Autocomplete from "./Autocomplete";
+import '../style/app.css';
 
 function App() {
+
   return (
     <div className="App">
+      <nav>
+        <h2 onClick={() => window.location.href = "/"}>Weather Cats</h2>
+        <button onClick={() => window.location.href = "/register"}>Register</button>
+        <Autocomplete />
+      </nav>
       <Outlet />
     </div>
   );
