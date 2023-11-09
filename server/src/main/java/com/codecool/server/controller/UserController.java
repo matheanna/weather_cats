48f3f79f458f40ae7c1bd9aaa36f08ebfd44554f
matheanna <113627409+matheanna@users.noreply.github.com>
2023-11-09 10:34:51 +0100

    refactor: user controller to work with security

--- server/src/main/java/com/codecool/server/controller/UserController.java
+++ server/src/main/java/com/codecool/server/controller/UserController.java
@@ -1,38 +1,47 @@
 package com.codecool.server.controller;
 
+import com.codecool.server.config.UserAuthProvider;
 import com.codecool.server.model.DTO.LoginDTO;
+import com.codecool.server.model.DTO.RegisterDTO;
 import com.codecool.server.model.DTO.UserDTO;
-import com.codecool.server.model.entity.User;
 import com.codecool.server.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
     private final UserService userService;
+    private final UserAuthProvider userAuthProvider;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, UserAuthProvider userAuthProvider) {
         this.userService = userService;
+        this.userAuthProvider = userAuthProvider;
     }
 
     @PostMapping("/register")
-    public User register(@RequestBody UserDTO userDTO){
-        User user = userService.register(userDTO);
-        return user;
+    public ResponseEntity<UserDTO> register(@RequestBody RegisterDTO registerDTO){
+        UserDTO user = userService.register(registerDTO);
+        user.setToken(userAuthProvider.createToken(user.getUsername()));
+        return ResponseEntity.created(URI.create("/users/" + user.getId())).body(user);
     }
 
     @PostMapping("/login")
-    public User login(@RequestBody LoginDTO loginDTO) throws Exception {
-        User user = userService.login(loginDTO);
-        return user;
+    public ResponseEntity<UserDTO> login(@RequestBody LoginDTO loginDTO) throws Exception {
+        UserDTO user = userService.login(loginDTO);
+        user.setToken(userAuthProvider.createToken(user.getUsername()));
+        return ResponseEntity.ok(user);
     }
 
 }
+
 
 
 
